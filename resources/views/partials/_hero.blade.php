4fbba59492475966feda4ff57170491e637724e0
Kim Miculob <kimmiculob.official@gmail.com>
2024-07-31 19:26:09 +0800

    first commit

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,10 @@
+<section class="relative h-32 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4" 
+style="background-color:#780000;">
+
+  <div class="z-10">
+
+    <p class="text-4xl text-gray-200 font-bold my-4">
+      MATCH YOU WITH DIFFERENT COMPANIES
+    </p>
+  </div>
+</section>
